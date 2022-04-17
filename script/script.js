27d7033790497296e62cb8b53903920f6cfd91ec
robertmenezesdev <rms.pessoal10@gmail.com>
2022-04-16 22:28:41 -0300

    adicionado novamente pasta do JS com ajuste de bugs

---
+++ script/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora < 12) {
+    img.src = './imagens/manhaEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Bom dia!`
+    document.body.style.background = '#25CCF7'
+  } else if (hora > 12 && hora < 18) {
+    img.src = './imagens/tardeEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Boa Tarde!`
+    document.body.style.background = '#F97F51'
+  } else {
+    img.src = './imagens/noiteEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Boa Noite!`
+    document.body.style.background = '#2C3A46'
+  }
+}
