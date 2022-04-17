1ff2b15bad6e6ce47fc8e4232af9db57ee108c3e
robertmenezesdev <rms.pessoal10@gmail.com>
2022-04-16 22:22:06 -0300

    add class ao body e refatoração

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,17 @@ function carregar() {
   var data = new Date()
   var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora < 12) {
+    img.src = './imagens/manhaEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Bom dia!`
+    document.body.style.background = '#25CCF7'
+  } else if (hora > 12 && hora < 18) {
+    img.src = './imagens/tardeEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Boa Tarde!`
+    document.body.style.background = '#F97F51'
+  } else {
+    img.src = './imagens/noiteEditada.png'
+    document.getElementById('mudarDia').innerHTML = `Boa Noite!`
+    document.body.style.background = '#2C3A47'
+  }
 }
