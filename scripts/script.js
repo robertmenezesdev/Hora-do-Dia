89f9aa70423728c2b417e952c33e927f70b916bc
robertmenezesdev <rms.pessoal10@gmail.com>
2022-04-16 22:18:55 -0300

    chama funcao new Date()

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-function carregar() {}
+function carregar() {
+  var msg = document.getElementById('msg')
+  var img = document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+}
